7a199c1bc5fae49b7d33ca8f513dc4ea0e801a74
misakisak <dr.misakisak@gmail.com>
2022-03-19 21:56:24 -0400

    misaki

---
+++ firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBzthk-GONr74LNbRIBswKl9O4jWNAevOc",
+  authDomain: "fir-auth-c4ecb.firebaseapp.com",
+  projectId: "fir-auth-c4ecb",
+  storageBucket: "fir-auth-c4ecb.appspot.com",
+  messagingSenderId: "720443880611",
+  appId: "1:720443880611:web:732d0e959d3d8c39b23b16"
+};
+
+// Initialize Firebase
+let app;
+if (firebase.getApps.length === 0) {
+    app = firebase.initializeApp(firebaseConfig);
+} else {
+    app = firebase.app()
+}
+
+const auth = firebase.auth()
+
+export { auth };
